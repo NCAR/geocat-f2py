9e2b90da966da449da725002aa5ee0e8217b4573
pilotchute <akootz@ucar.edu>
2020-08-28 14:46:43 -0600

    updated dlinint2pts to not be broken:
    Signed-off-by: pilotchute <akootz@ucar.edu>

--- src/geocat/temp/fortran/linint2_wrapper.py
+++ src/geocat/temp/fortran/linint2_wrapper.py
@@ -3,14 +3,14 @@ import numpy as np
 
 n = 127
 
-xi = np.linspace(1,10,11)
-yi = np.linspace(1,10,11)
-xo = np.linspace(1,10,22)
-yo = np.linspace(1,10,22)
-fi_np = np.random.rand(len(yi), len(xi)).astype(np.float64)
+xi = np.linspace(1,10,10)
+yi = np.linspace(1,10,20)
+xo = np.linspace(1,10,20)
+yo = np.linspace(1,10,40)
+fi = np.linspace(1, 200, 200).reshape((10, 20))
 
+print(fi)
 
-
-fo = dlinint2(xi,yi,fi_np,xo,yo)
+fo = dlinint2(xi, yi, fi, xo, yo)
 
 print(fo)
