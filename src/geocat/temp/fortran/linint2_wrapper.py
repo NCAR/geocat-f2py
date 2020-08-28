e3f6adf41b76b7a919eef437fbc60fbab45b16c7
pilotchute <akootz@ucar.edu>
2020-08-28 14:27:36 -0600

    IT'S WORKING
    Signed-off-by: pilotchute <akootz@ucar.edu>

--- src/geocat/temp/fortran/linint2_wrapper.py
+++ src/geocat/temp/fortran/linint2_wrapper.py
@@ -1,1 +1,16 @@
 from linint2 import (dlinint1, dlinint2, dlinint2pts)
+import numpy as np
+
+n = 127
+
+xi = np.linspace(1,10,11)
+yi = np.linspace(1,10,11)
+xo = np.linspace(1,10,22)
+yo = np.linspace(1,10,22)
+fi_np = np.random.rand(len(yi), len(xi)).astype(np.float64)
+
+
+
+fo = dlinint2(xi,yi,fi_np,xo,yo)
+
+print(fo)
