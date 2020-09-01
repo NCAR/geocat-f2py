43a9ea86c2acfab9db43759a4f8d13bbaa721a72
pilotchute <akootz@ucar.edu>
2020-09-01 10:23:03 -0600

    it's working (again)
    Signed-off-by: pilotchute <akootz@ucar.edu>

--- src/geocat/temp/fortran/__init__.py
+++ src/geocat/temp/fortran/__init__.py
@@ -1,1 +1,1 @@
-from .linint2 import( dlinint1, dlinint2, dlinint2)
+from .linint2 import( dlinint1, dlinint2, dlinint2pts)
--- src/geocat/temp/linint2_wrapper.py
+++ src/geocat/temp/linint2_wrapper.py
@@ -1,8 +1,52 @@
 import numpy as np
 import xarray as xr
-from .fortran import (dlinint2)
+
+from geocat.temp.fortran import (dlinint2)
 
 
-def linint2(fi, xo, yo):
+def linint2(fi, xo, yo, xi=None, yi=None):
+    # '''
+    if not isinstance(fi, xr.DataArray):
+        fi = xr.DataArray(fi)
+
+    if xi is None:
+        xi = fi.coords[fi.dims[-2]]
+
+    if yi is None:
+        yi = fi.coords[fi.dims[-1]]
+
+    # '''
+
     fo = dlinint2(xi, yi, fi, xo, yo)
     return fo
+
+
+def linint2_tests():
+    n = 127
+
+    xi = np.linspace(1, 10, 10)
+    yi = np.linspace(1, 10, 20)
+    xo = np.linspace(1, 10, 20)
+    yo = np.linspace(1, 10, 40)
+    fi = np.linspace(1, 200, 200).reshape((1, 10, 20))
+    '''
+    chunks = {
+        'alt': fi.shape[0],
+        'lat': fi.shape[1],
+        'lon': fi.shape[2],
+    }
+    #'''
+
+    fi = xr.DataArray(fi,
+                      dims=['alt', 'lat', 'lon'],
+                      coords={
+                          'lat': xi,
+                          'lon': yi,
+                      },
+                      )
+    fo = linint2(fi, xo, yo)
+
+    print(fo)
+
+
+linint2_tests()
