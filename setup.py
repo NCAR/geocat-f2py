574998aef9faa0fe5b4a479f0995d94457d45b03
austin kootz <austin@kootz.com>
2020-09-09 20:19:37 -0600

    file backbone built for conda-build
    Signed-off-by: austin kootz <austin@kootz.com>

---
+++ setup.py
@@ -0,0 +1,19 @@
+#''' setup.py is needed, but only to make namespaces happen, 
+version = '0.1'
+
+from setuptools import setup
+
+setup(
+      name='geocat.temp',
+      version=version,
+      package_dir={
+                   '': 'src',
+                   'geocat': 'src/geocat',
+                   'geocat.temp': 'src/geocat/temp',
+                   'geocat.temp.fortran': 'src/geocat/temp/fortran',
+                   }
+      namespace_packages=['geocat'],
+      packages=['geocat','geocat.temp','geocat.temp.fortran'],
+      install_requires=['numpy','dask[complete]','xarray'],
+      )
+
