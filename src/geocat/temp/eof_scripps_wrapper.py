966a9ec5db389029e184904be43a79bbb23f9406
pilotchute <akootz@ucar.edu>
2020-10-21 16:50:45 -0600

    eof scripps up and running for eof11
    Signed-off-by: pilotchute <akootz@ucar.edu>

--- src/geocat/temp/eof_scripps_wrapper.py
+++ src/geocat/temp/eof_scripps_wrapper.py
@@ -1,6 +1,7 @@
 import numpy as np
 import xarray as xr
 from dask.array.core import map_blocks
+import time
 
 from geocat.temp.fortran import (deof11)
 
@@ -9,7 +10,21 @@ from geocat.temp.fortran import (deof11)
 # These Wrapper are executed within dask processes, and should do anything that
 # can benefit from parallel excution.
 
-def _eof11():
+def _eof11(d, nmodes, icovcor, dmsg):
     #''' eigenvalues,eigenvectors,variance,princomp = deof11(d,nmodes,[icovcor,dmsg])
+    eigenvalues, eigenvectors, variance, princomp = deof11(d,nmodes,[icovcor,dmsg])
+    return eigenvalues, eigenvectors, variance, princomp
 
+def eof11(d, nmodes, icovcor=0, dmsg=-99):
+    # this is effectivly a stub, since this is not a parallelizable task.
+    return _eof11(d, nmodes, icovcor, dmsg)
 
+def eof_test():
+    d = np.random.random((1000,10000))
+    nmodes = 10
+    print(eof11(d, nmodes))
+
+start = time.time()
+eof_test()
+end = time.time()
+print(end-start)
