a7f6a6943114180f9515052dbb1fc38790f3120e
pilotchute <akootz@ucar.edu>
2020-10-26 12:50:21 -0600

    last tweak before merge to master
    Signed-off-by: pilotchute <akootz@ucar.edu>

--- src/geocat/temp/fortran/__init__.py
+++ src/geocat/temp/fortran/__init__.py
@@ -1,2 +1,3 @@
-from .linint2 import(dlinint1, dlinint2, dlinint2pts)
-from .rcm2rgrid import(drcm2rgrid, drgrid2rcm)
+from .eof_scripps import (deof11)
+from .linint2 import (dlinint1, dlinint2, dlinint2pts)
+from .rcm2rgrid import (drcm2rgrid, drgrid2rcm)
