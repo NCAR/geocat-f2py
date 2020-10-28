e112fe968895005846345e587cbfea1636e9be24
erogluorhan <erogluorhan@gmail.com>
2020-10-28 14:52:27 -0600

    Removed the debug lines forgotten at the end of a test file

--- test/test_linint2.py
+++ test/test_linint2.py
@@ -294,6 +294,3 @@ class Test_linint2_non_contiguous(ut.TestCase):
                                   yi=yi_reverse[::-1])
         np.testing.assert_array_equal(fi[:, :, ::-1, :].values,
                                       fo[..., ::2, ::2].values)
-
-    a = Test_linint2_int32()
-    a.test_linint2_int32()
