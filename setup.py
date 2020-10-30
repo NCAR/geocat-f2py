2d81778cfd840e6fc986cc8907c78fbbbcdc57a9
erogluorhan <erogluorhan@gmail.com>
2020-10-30 14:18:52 -0600

    Fixed recursive directory import problem for geocat.temp.fortran with setup.py

--- setup.py
+++ setup.py
@@ -3,6 +3,15 @@ version = '0.1'
 
 from setuptools import setup, find_packages
 
+
+
+from pathlib import Path
+
+datadir = Path(__file__).parent / 'src'/ 'geocat' / 'temp' / 'fortran'
+files = [str(p.relative_to(datadir)) for p in datadir.rglob('*.so')]
+
+
+
 setup(
       name='geocat.temp',
       version=version,
@@ -12,7 +21,8 @@ setup(
                    'geocat.temp': 'src/geocat/temp',
                    'geocat.temp.fortran': 'src/geocat/temp/fortran',
                    },
+      package_data={'geocat.temp.fortran': files},
       namespace_packages=['geocat'],
-      packages=['geocat','geocat.temp','geocat.temp.fortran'],
+      packages=['geocat','geocat.temp', 'geocat.temp.fortran'],
       )
 
