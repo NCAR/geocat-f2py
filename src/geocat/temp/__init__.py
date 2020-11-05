46e175ebb03afbe0bdacfe7264b09894965eab94
anissa111 <anissaz@ucar.edu>
2020-11-04 21:35:37 -0700

    pull to clean branch for merge

--- src/geocat/temp/__init__.py
+++ src/geocat/temp/__init__.py
@@ -1,4 +1,5 @@
 from .errors import *
 from .linint2_wrapper import (linint1, linint2, linint2pts)
 from .rcm2rgrid_wrapper import (rcm2rgrid, rgrid2rcm)
+from .moc_globe_atl_wrapper import moc_globe_atl
 from .missing_values import (py2fort_msg, fort2py_msg)
