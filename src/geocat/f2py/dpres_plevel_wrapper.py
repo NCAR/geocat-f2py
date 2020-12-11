3cb439b45ea878ac80d9102552efb412636fa151
erogluorhan <erogluorhan@gmail.com>
2020-12-11 15:55:47 -0700

    Added sanity checks and refactored a bit

--- src/geocat/f2py/dpres_plevel_wrapper.py
+++ src/geocat/f2py/dpres_plevel_wrapper.py
@@ -6,36 +6,6 @@ from .errors import (DimensionError, AttributeError)
 from .missing_values import (fort2py_msg, py2fort_msg)
 
 
-# Inner Wrapper _<funcname>()
-# This wrapper handles transpose of the input and output data
-# as well as missing value representations before
-# and after the Fortran function call.
-
-
-def _dpres_plevel(plev, psfc, ptop, msg_py):
-
-    # Transpose psfc before Fortran function call
-    if psfc.ndim == 2:
-        psfc = np.transpose(psfc, axes=(1,0))
-    elif psfc.ndim == 3:
-        psfc = np.transpose(psfc, axes=(2,1,0))
-
-    # Handle Python2Fortran missing value conversion
-    plev, msg_py, msg_fort = py2fort_msg(plev, msg_py=msg_py)
-
-    # Fortran function call
-    dp = dpresplvl(plev, psfc, ptop, pmsg=msg_fort)
-
-    # Transpose output before returning it to outer wrapper
-    dp = np.asarray(dp)
-    dp = np.transpose(dp, axes=(3,2,1,0))
-
-    # Handle Fortran2Python missing value conversion back
-    fort2py_msg(dp, msg_fort=msg_fort, msg_py=msg_py)
-
-    return dp
-
-
 # Outer Wrapper <funcname>()
 # These Wrappers are excecuted in the __main__ python process, and should be
 # used for any tasks which would not benefit from parallel execution.
@@ -111,7 +81,51 @@ def dpres_plevel(plev, psfc, ptop=None, msg_py=None, meta=False):
                     result_dp = geocat.comp.dpres_plevel(plev, psfc, ptop)
             """
 
-    # ''' Start of boilerplate
+    plev, psfc, ptop = sanity_check(plev, psfc, ptop)
+
+    # Inner wrapper call
+    dp = _dpres_plevel(plev.values, psfc.values, ptop, msg_py)
+
+    if psfc.values.ndim == 1:
+        dp = dp.reshape(dp.shape[1])
+    elif psfc.values.ndim == 2:
+        dp = dp.reshape(dp.shape[1], dp.shape[2], dp.shape[3])
+
+    dp = xr.DataArray(dp)
+
+    return dp
+
+
+# Inner Wrapper _<funcname>()
+# This wrapper handles transpose of the input and output data
+# as well as missing value representations before
+# and after the Fortran function call.
+
+def _dpres_plevel(plev, psfc, ptop, msg_py):
+
+    # Transpose psfc before Fortran function call
+    if psfc.ndim == 2:
+        psfc = np.transpose(psfc, axes=(1,0))
+    elif psfc.ndim == 3:
+        psfc = np.transpose(psfc, axes=(2,1,0))
+
+    # Handle Python2Fortran missing value conversion
+    plev, msg_py, msg_fort = py2fort_msg(plev, msg_py=msg_py)
+
+    # Fortran function call
+    dp = dpresplvl(plev, psfc, ptop, pmsg=msg_fort)
+
+    # Transpose output before returning it to outer wrapper
+    dp = np.asarray(dp)
+    dp = np.transpose(dp, axes=(3,2,1,0))
+
+    # Handle Fortran2Python missing value conversion back
+    fort2py_msg(dp, msg_fort=msg_fort, msg_py=msg_py)
+
+    return dp
+
+def sanity_check(plev, psfc, ptop):
+    # ''' Basic sanity checks
     if not isinstance(plev, xr.DataArray):
         plev = xr.DataArray(plev)
 
@@ -120,7 +134,7 @@ def dpres_plevel(plev, psfc, ptop=None, msg_py=None, meta=False):
             "ERROR dpres_plevel: The 'plev' array must be 1 dimensional array !"
         )
 
-    if np.size(psfc) == 1:  # if it is a scalar, then construct an xaray.dataarray
+    if np.size(psfc) == 1:  # if it is a scalar, then construct an xarray.dataarray
         psfc = np.asarray(psfc)
         psfc = np.ndarray([1], buffer=psfc, dtype=psfc.dtype)
         psfc = xr.DataArray(psfc)
@@ -132,13 +146,13 @@ def dpres_plevel(plev, psfc, ptop=None, msg_py=None, meta=False):
             "ERROR dpres_plevel: 'psfc' must be a scalar, or 2 or 3 dimensional array with right most dimensions lat x lon !"
         )
 
-    # plev and psfc must have the same units.
-    if "units" in plev.attrs.values() and "units" in psfc.attrs.values():
+    # plev and psfc must have the same units, if they have any.
+    if "units" in plev.attrs.keys() and "units" in psfc.attrs.keys():
         if plev.attrs["units"] != psfc.attrs["units"]:
             raise AttributeError(
                 "ERROR dpres_plevel: Units of 'plev' and 'psfc' needs to match !"
             )
-    elif "units" in plev.attrs.values() or "units" in psfc.attrs.values():
+    elif "units" in plev.attrs.keys() or "units" in psfc.attrs.keys():
         raise AttributeError(
             "ERROR dpres_plevel: Either of 'plev' and 'psfc' has ""units"" attribute but the other does not !"
         )
@@ -150,22 +164,9 @@ def dpres_plevel(plev, psfc, ptop=None, msg_py=None, meta=False):
 
     if ptop is None:
         ptop = min(plev)
-    else:
+    else:git
         if ptop > min(plev):
             raise ValueError(
                 "ERROR dpres_plevel: The 'ptop' value must be <= min(plev) !")
 
-    dp = _dpres_plevel(plev.values, psfc.values, ptop, msg_py)
-
-    if psfc.values.ndim == 1:
-        dp = dp.reshape(dp.shape[1])
-    elif psfc.values.ndim == 2:
-        dp = dp.reshape(dp.shape[1], dp.shape[2], dp.shape[3])
-
-
-
-    dp = xr.DataArray(dp)
-
-
-
-    return dp
+    return plev, psfc, ptop
