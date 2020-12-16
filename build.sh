fe437334deeb0200f661f4c5672675e0758960b1
pilotchute <akootz@ucar.edu>
2020-12-16 16:11:00 -0700

    ddrveof compiling
    Signed-off-by: pilotchute <akootz@ucar.edu>

--- build.sh
+++ build.sh
@@ -5,6 +5,7 @@ f2py -c --fcompiler=gnu95 linint2.pyf linint2.f
 f2py -c --fcompiler=gnu95 rcm2rgrid.pyf rcm2rgrid.f linmsg_dp.f linint2.f
 f2py -c -llapack --fcompiler=gnu95 eof_scripps.pyf eof_scripps.f90
 f2py -c -llapack --fcompiler=gnu95 prneofts_dp.pyf prneofts_dp.f
+f2py -c -llapack --fcompiler=gnu95 prneof_dp.pyf prneof_dp.f
 f2py -c --fcompiler=gnu95 moc_loops.pyf moc_loops.f
 cd ../../../..
 
