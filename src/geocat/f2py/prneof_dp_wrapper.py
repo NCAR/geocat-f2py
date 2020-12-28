8be835fd17308f21dba283ac9f7783641eab374a
pilotchute <akootz@ucar.edu>
2020-12-28 13:00:50 -0700

    prneof_dp_wrapper.py done
    Signed-off-by: pilotchute <akootz@ucar.edu>

--- src/geocat/f2py/prneof_dp_wrapper.py
+++ src/geocat/f2py/prneof_dp_wrapper.py
@@ -22,3 +22,17 @@ def _drveof(x, nobs, msta, msg_py, neval, jopt):
     x, msg_py, msg_fort = fort2py_msg(x, msg_fort=msg_fort, msg_py=msg_py)
 
     return eval, evec, pcvar, trace
+
+def drveof(x, nobs=None, msta=None, msg_py=None, neval=None, jopt=None):
+    # handle optionals
+    if nobs is None:
+        nobs = x.shape[0]
+    if msta is None:
+        msta = x.shape[1]
+    if neval is None:
+        neval = msta
+    if jopt is None:
+        jopt = 0;
+
+    # return inner computation. No Dask call, as this is not parallelizable
+    return _drveof(x, nobs, msta, msg_py, neval, jopt)
