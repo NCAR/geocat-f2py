8ecdc47a8ba53b15d2e388417b588d8c3d20c697
erogluorhan <erogluorhan@gmail.com>
2021-02-25 12:34:35 -0700

    Updated conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -12,6 +12,7 @@
 #
 import os
 import sys
+import geocat.f2py
 # sys.path.insert(0, os.path.abspath('.'))
 
 
@@ -35,6 +36,14 @@ version = read_version()
 release = read_version()
 
 # -- General configuration ---------------------------------------------------
+
+# The master toctree document.
+master_doc = 'index'
+
+# Add any paths that contain custom static files (such as style sheets) here,
+# relative to this directory. They are copied after the builtin static files,
+# so a file named "default.css" will overwrite the builtin "default.css".
+html_static_path = ['_static']
 
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
