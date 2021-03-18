2ac30c98ee3e99d2cfb929db52f95af14d42971a
michaelavs <michaela.sizemore@gmail.com>
2021-03-18 10:29:15 -0600

    Updating docstring, removed some redundant lines

--- src/geocat/f2py/checks.py
+++ src/geocat/f2py/checks.py
@@ -6,7 +6,7 @@ import dask as da
 Checks wrapper functions to ensure data is correctly formatted and stops code from running if not.
 '''
 
-def check(user_variable_1,
+def check(user_variable,
           var_name='variable',
           data_type=None,
           max_dimensions=None,
@@ -17,57 +17,79 @@ def check(user_variable_1,
           is_none=None,
           comparison=None,
           unchunked_dims=None):
+    '''
+    Function of data checks for use in wrapper functions.
 
-    if data_type is not None:
-        ''' 
-        data_type input of type or tuple of types
+    user_variable : :class:`xarray.DataArray` or :class:`numpy.ndarray`:
+        Variable of interest
+
+    data_type : :class:`type` or :type:`tuple of types`:
+        Desired data_type of user_variable
         ex: data_type=np.ndarray
-        '''
-        if not isinstance(user_variable_1, data_type):
+
+    max_dimensions : :class:`tuple`:
+        Maximum number of dimensions needed from user_variable for function
+        ex: min_dimensions=(3) or min_dimensions=3
+
+    min_dimensions : :class:`tuple`:
+        Minimum number of dimensions needed from user_variable for function
+        ex: min_dimensions=(3) or min_dimensions=3
+
+    dimensions : :class:`tuple`:
+        Number of dimensions needed from user_variable for function
+        ex: min_dimensions=(3) or min_dimensions=3
+
+    shape : :class:`tuple`:
+        Checks user_variable's shape
+        ex: shape=(3, 3, 3) or shape=(3,3,3)
+
+    is_xarray : :class:`bool`:
+        Checking that user_variable is of class xarray.DataArray.
+        Valid inputs are True or False
+
+    is_none : :class:`NoneType`:
+        Checking that user_variable is of class NoneType.
+        Valid input is None
+
+    comparison : :class:`int`:
+        Checks that user_variable is the correct type
+        ex: check(user_variable, comparison=
+
+    unchunked_dims : :class:`tuple`:
+        Checks for unchunked dimensions in user_variable
+        ex: check(user_variable, unchunked_dims=[3]) *must include brackets*
+
+    '''
+    if data_type is not None:
+        if not isinstance(user_variable, data_type):
             raise Exception(var_name + " failed data_type check")
 
     if dimensions is not None:
-        '''
-        Number of dimensions of variable in type tuple
-        ex: dimensions=(3) or dimensions=3
-        '''
-        if not (user_variable_1.ndim == dimensions):
+        if not (user_variable.ndim == dimensions):
             raise Exception(var_name + " failed dimensions check")
 
     if shape is not None:
-        '''
-        Input variable's shape of type tuple
-        ex: shape=(3, 3, 3) or shape=(3,3,3)
-        '''
-        if not (user_variable_1.shape == shape):
+        if not (user_variable.shape == shape):
             raise Exception(var_name + " failed shape check")
 
     if is_none is not None:
         if is_none is True:
-            if user_variable_1 is not None:
+            if user_variable is not None:
                 raise Exception(var_name + " is not None")
         if is_none is False:
-            if user_variable_1 is None:
+            if user_variable is None:
                 raise Exception(var_name + " is None")
 
     if min_dimensions is not None:
-        '''
-        Minimum number of dimensions needed from variable for function in type tuple
-        ex: min_dimensions=(3) or min_dimensions=3
-        '''
-        if not (user_variable_1.ndim >= min_dimensions):
+        if not (user_variable.ndim >= min_dimensions):
             raise Exception(var_name + " failed min_dimensions check")
 
     if max_dimensions is not None:
-        '''
-        Maximum number of dimensions allowed from variable for function in type tuple
-        ex: max_dimensions=(3) or max_dimensions=3
-        '''
-        if not (user_variable_1.ndim <= max_dimensions):
+        if not (user_variable.ndim <= max_dimensions):
             raise Exception(var_name + " failed max_dimensions check")
 
     if comparison is not None:
-        if not (user_variable_1 == comparison):
+        if not (user_variable == comparison):
             raise Exception(var_name + " failed comparison check")
 
     #
@@ -75,32 +97,22 @@ def check(user_variable_1,
     #
 
     if is_xarray is not None:
-        '''
-        Checking that input data is of class xarray.DataArray. 
-        Input is True or False
-        '''
         if is_xarray is True:
-            if not isinstance(user_variable_1, xr.DataArray):
-                print(var_name + " is not xarray.DataArray, reformatting...")
+            if not isinstance(user_variable, xr.DataArray):
+                # print(var_name + " is not xarray.DataArray, reformatting...")
                 return False
-                # raise Exception(var_name + " is not a xarray.DataArray")
             else: pass
         if is_xarray is False:
-            if isinstance(user_variable_1, xr.DataArray):
+            if isinstance(user_variable, xr.DataArray):
                 pass
                 # raise Exception(var_name + " is an xarray.DataArray")
             else:
-                print(var_name + " is not xarray.DataArray, reformatting...")
+                # print(var_name + " is not xarray.DataArray, reformatting...")
                 return True
 
     if unchunked_dims is not None:
-        '''
-        Checking that input data is chunked correctly. 
-        Input is type tuple
-        ex: unchunked_dims=[3] *must include brackets*
-        '''
         for dim in unchunked_dims:
-            if len(user_variable_1.chunks[dim]) > 1:
+            if len(user_variable.chunks[dim]) > 1:
                 raise Exception(var_name +
                                 " must not be chunked along dimension " +
                                 str(dim))
