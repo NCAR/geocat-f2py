60b871038078aebb82198420e829aea88b92172b
erogluorhan <erogluorhan@gmail.com>
2021-03-23 19:54:00 -0600

    trigger_build_test

--- docs/conf.py
+++ docs/conf.py
@@ -13,7 +13,7 @@
 import os
 import sys
 
-sys.path.insert(0, os.path.abspath('..'))
+sys.path.insert(0, os.path.abspath('../src/geocat/f2py'))
 
 sys.path.insert(0, os.path.abspath('.'))
 import subprocess
